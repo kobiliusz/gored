ddbcba9cba300578f6354fab09f4fb4cff940245
kobiliusz <kobiliusz@gmail.com>
2021-02-25 23:02:48 +0100

    added flows listing

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 )
 
+type flow struct {
+	Url      string      `json:"url"`
+	Name     string      `json:"name"`
+	Function interface{} `json:"-"`
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	flows := []flow{
+		flow{
+			Url:      "/lights_on",
+			Name:     "Lights On",
+			Function: lightson,
+		},
+		flow{
+			Url:      "/lights_off",
+			Name:     "Lights Off",
+			Function: lightsoff,
+		},
+	}
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":6666")
+
+	router.GET("/flows", func(c *gin.Context) {
+		json.NewEncoder(c.Writer).Encode(&flows)
+	})
+
+	router.Run(":6666")
+}
+
+func lightson() {
+
+}
+
+func lightsoff() {
+
 }
